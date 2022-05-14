43f4387c3d826850d340fe4575235463ea04d8df
Isabella Stephens <stephens.isabella@gmail.com>
2022-05-14 13:43:48 -0700

    Adding another hackerrank problem

---
+++ hackerrank/old/c/1darrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	int n, i, total = 0;
+	scanf("%d", &n);
+	while(n--) {
+		scanf("%d", &i);
+		total += i;
+	}
+
+	printf("%d", i);
+
+	return 0;
+}
